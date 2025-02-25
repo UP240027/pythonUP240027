4a36cd0d4b15499fcad8ded3df3d0a4788d1077b
UP240027 <up240027@upa.alumnos.edu.mx>
2025-02-25 08:28:48 -0600

    .

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -5,9 +5,22 @@ print (sentence0)
 sentence1 = 'Codificacion para todos' 
 print (sentence1)
 #3-Declare a variable named company and assign it to an initial value "Coding For All".
-company = 'Codificacion para todos'
+company = 'codificacion para todos'
 #4-Print the variable company using print().
 print (company)
 #5-Print the length of the company string using len() method and print().
-print("La longitud de company es:",len('company'))
-#Change all the characters to uppercase letters using upper() method.
+print("La longitud de company es:",len(company))
+#6-ange all the characters to uppercase letters using upper() method.
+company = 'codificacion para todos'
+print(company.upper()) 
+#7-Change all the characters to lowercase letters using lower() method.company = 'codificacion para todos'
+company = 'CODIFICACION PARA TODOS'
+print(company.lower()) 
+#8-Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+company = 'codificacion para todos'
+print(company.capitalize()) 
+company = 'codificacion para todos'
+print(company.title())
+company = 'codificacion para todos'
+print(company.swapcase())
+#9-Cut(slice) out the first word of Coding For All string.
