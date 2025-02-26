33587d30c2fb4d500bf3f9e3cea2f51fb644cbb8
UP240027 <up240027@upa.alumnos.edu.mx>
2025-02-26 10:00:07 -0600

    :)

---
+++ EjercisioDia5.py
@@ -0,0 +1,22 @@
+#1-Declare an empty list
+lista_vacia = []
+print(len(lista_vacia))
+
+#2-Declare a list with more than 5 items/ lista con 5 componentes
+colores = ['azul', 'verde', 'rosa', 'amarillo', 'rojo','morado']
+print ("Los colores de la lista son:", colores)
+
+#3-Find the length of your list/ cuantos componentes tiene la lista
+print("La longitud/componentes de la lista de colores es:", len(colores))
+
+#4-Get the first item, the middle item and the last item of the list/ contando desde 0 los elementos de la lista
+primer_color_de_la_litsta = colores[0]
+print("El primer color de la lista es", primer_color_de_la_litsta)
+color_de_en_medio_de_la_lista = colores[2]
+print("El color de en medio de la lista es:", color_de_en_medio_de_la_lista)
+ultimo_color_de_la_lista = colores[5]
+print("El ultimo color de la lista es:", ultimo_color_de_la_lista)
+
+#5-Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print("Las empresas de la lista son:", it_companies)
