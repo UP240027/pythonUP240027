203557ef14fa6ce466b278e4a0e693c9b744cecd
UP240027 <up240027@upa.alumnos.edu.mx>
2025-02-26 10:11:27 -0600

    .

--- EjercisioDia5.py
+++ EjercisioDia5.py
@@ -1,6 +1,6 @@
 #1-Declare an empty list
 lista_vacia = []
-print(len(lista_vacia))
+print(len(lista_vacia)) #comrpueba la lista vacia
 
 #2-Declare a list with more than 5 items/ lista con 5 componentes
 colores = ['azul', 'verde', 'rosa', 'amarillo', 'rojo','morado']
@@ -17,6 +17,26 @@ print("El color de en medio de la lista es:", color_de_en_medio_de_la_lista)
 ultimo_color_de_la_lista = colores[5]
 print("El ultimo color de la lista es:", ultimo_color_de_la_lista)
 
-#5-Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+#5-Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Jessica', 18, 1.60, False, 'Aguascalientes']
+print("Los datos de la lista son:", mixed_data_types)   
+
+
+#6-Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
 it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
 print("Las empresas de la lista son:", it_companies)
+
+#7-Print the list using print()
+print(it_companies)
+
+#8-Print the number of companies in the list
+print("El numero de empresas que hay en la lista es:", len(it_companies))
+
+#9-Print the first, middle and last company
+primer_empresa =it_companies[0]
+print("La primera empresa de la lista es:", primer_empresa)
+empresa_del_medio = it_companies[3]
+print("La empresa del medio de la lista es", empresa_del_medio)
+ultima_empresa = it_companies[6]
+print("La ultima empresa de la lista es:", ultima_empresa)
+
