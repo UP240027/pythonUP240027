dffd70bec14380008a68e69129b958f4209c6d08
UP240027 <up240027@upa.alumnos.edu.mx>
2025-03-04 08:36:09 -0600

    .

---
+++ EjercisioDia6.py
@@ -0,0 +1,44 @@
+#Excercises level 1
+#1-Create an empty tuple
+tupla_vacia = ()
+tupla_vacia = tuple ()
+
+
+#2- Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+tpl1 = ('Jacquline', 'Christopher')
+
+#3-Join brothers and sisters tuples and assign it to siblings
+tpl1 = ('Jacquline', 'Christopher','Hola')
+tpl2 = ('Britny','Dariana','LOlita')
+tpl3 = tpl1 + tpl2
+print(tpl3)
+
+
+#4-How many siblings do you have?
+len (tpl2)
+print ("El numero de primos son:", len(tpl2))
+
+#5-Modify the siblings tuple and add the name of your father and mother and assign it to family_member
+tpl2 = ('Britny','Dariana','LOlita','Vero','Daniel')
+miembros_de_la_familia= tpl1 + tpl2
+print("Los miembros de la familia son:", miembros_de_la_familia)
+
+# Exercises: Level 2
+# Unpack siblings and parents from family_members
+
+# Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
+frutas =('manzana','pera','platano')
+vegetales = ('zanahoria','lechuga','espinaca')
+productos_animales = ('pollo','res','pescado')
+food_stuff_tp = frutas + vegetales + productos_animales 
+print("Las tuplas unidas se ven asi:", food_stuff_tp)
+
+# Change the about food_stuff_tp tuple to a food_stuff_lt list
+# Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+# Slice out the first three items and the last three items from food_staff_lt list
+# Delete the food_staff_tp tuple completely
+# Check if an item exists in tuple:
+# Check if 'Estonia' is a nordic country
+
+# Check if 'Iceland' is a nordic country
+
