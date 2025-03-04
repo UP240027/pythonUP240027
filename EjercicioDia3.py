022cb2f26aaa17acf46c4293543a35a7e0863805
UP240027 <up240027@upa.alumnos.edu.mx>
2025-03-04 08:09:32 -0600

    .

--- EjercicioDia3.py
+++ EjercicioDia3.py
@@ -51,16 +51,17 @@ xc = (y + 2)/2
 print("El valor de x es: ", xc)
 print('El valor de la pendiente es: (' + str(xc) +' , '+ str(yc)+')')
 
-## 9-Encontrar la pendiente y y la distancia euclidiana entre el punto (2, 2) y el punto (6, 10).
+## Encontrar la pendiente y y la distancia euclidiana entre el punto (2, 2) y el punto (6, 10).
 x1 = 2
- y1 =2
- x2 = 6
- y2 = 10
- m = (y2-y1)/(x2-x1)
- print ('La pendiente es: ', m)
- d = ((x2 - x1)*2 + (y2 - y1)*2)
- res = d**0.5
- print ('La distancia euclidiana es de:', res)
+y1 =2
+x2 = 6
+y2 = 10
+m = (y2-y1)/(x2-x1)
+print ('La pendiente es: ', m)
+d = ((x2 - x1)*2 + (y2 - y1)*2)
+res = d**0.5
+print ('La distancia euclidiana es de: ',res)
+
 
 #10-comparar pendientes. PENDIENTE
 
@@ -102,17 +103,54 @@ print('El numero de letras que tiene python es: ', len('python'))
 print (type(float(len(palabras))))
 
 #17-Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+numero1 = 4
+numero2 = 6
+numero3 = 7
+numero4 = 25
+frase1 = 'Divisible entre 2'
+frase2 = 'No es divisible entre 2 y el resultado es 0 '
+sum = numero1/2
+print('El numero es: ', frase1, sum )
+sum2 = numero2/2
+print('El numero es: ', frase1, sum2 )
+sum3 = numero3/2
+print('El numero es: ', frase2, 0)
+sum4 = numero4/2
+print('El numero es: ', frase2, 0)
 
 # #18-Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+num = 7
+div = 3
+division = num/div
+print(type(int(division)))
+print('El resultado de la division no es 2,7: ', not True)
+print('El valor de la division es: ', int(division))
+
 # prueba = (7//3== int(2.7))
 # print(prueba) 
 
 # #19-Check if type of '10' is equal to type of 10
 # resultado = (type('10') == type(10))
 # print(resultado) 
+print('El tipo de 10 es igual al tipo de 10: ', type(10) == type(10))
+
 
 #20-Check if int('9.8') is equal to 10
+valor = '9.8'
+print(type(float(valor)))
+print('El valor del numero es: ', valor)
+print('El valor de 9.8 es igual a 10: ', not True)
+
 
 #22-Writ a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+#calcular el salario de la persona?
+horas = float(input('Ingrese las horas trabajadas: '))
+tarifa = float(input('Ingrese la tarifa por hora: '))
+salario = horas * tarifa
+print('El salario de la persona es de: ', salario)
+
 
 #23-Write a Python script that displays the following table
+años = float(input('Ingrese la cantidad de años: '))
+segundos = años * 365 * 24 * 60 * 60
+print('La cantidad de segundos que puede vivir una persona es de: ',segundos)
