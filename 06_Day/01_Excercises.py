efe55ebf36135bce6ca1e88c2f1efe94eb44636f
UP240027 <up240027@upa.alumnos.edu.mx>
2025-03-12 10:04:11 -0600

    .

---
+++ 06_Day/01_Excercises.py
@@ -0,0 +1,25 @@
+#Excercises level 1
+#1-Create an empty tuple
+tupla_vacia = ()
+tupla_vacia = tuple ()
+
+
+#2- Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+tpl1 = ('Jacquline', 'Christopher')
+
+#3-Join brothers and sisters tuples and assign it to siblings
+tpl1 = ('Jacquline', 'Christopher','Hola')
+tpl2 = ('Britny','Dariana','LOlita')
+tpl3 = tpl1 + tpl2
+print(tpl3)
+
+
+#4-How many siblings do you have?
+len (tpl2)
+print ("El numero de primos son:", len(tpl2))
+
+#5-Modify the siblings tuple and add the name of your father and mother and assign it to family_member
+tpl2 = ('Britny','Dariana','LOlita','Vero','Daniel')
+miembros_de_la_familia= tpl1 + tpl2
+print("Los miembros de la familia son:", miembros_de_la_familia)
+
