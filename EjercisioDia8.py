91d63a91f208f33eb6ae8f9493c9a2550aec1854
UP240027 <up240027@upa.alumnos.edu.mx>
2025-03-12 09:40:15 -0600

    .

--- EjercisioDia8.py
+++ EjercisioDia8.py
@@ -4,27 +4,103 @@ dog = {}
 dog =  {'white':'beagle', '4 legs':'3 years'}
 print ("La descripcion de el diccionario perro queda asi:",dog)
 #Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
-student = {
+estudiante = {
     'first_name':'Jessica',
     'last_name':'Mejia',
     'age':18,
     'country':'Mexico',
     'is_marred':False,
-    'skills':{'elemento1': 'elemento2', 'elemnto3':'elemento4',},
+    'skills':{'elemento1': 'elemento2'},
     'address':{
         'street':'Space street',
         'zipcode':'02210'
     }
     }
-print("La descripcion del estudiante queda asi:", student)
+print("La descripcion del estudiante queda asi:", estudiante)
 
 #Get the length of the student dictionary
-print("La cantidad de datos del estudiante son:", len(student))
+print("La cantidad de datos del estudiante son:", len(estudiante))
 
 #Get the value of skills and check the data type, it should be a list
 skills = {'elemento1','elemento2', 'elemnto3','elemento4',}
 print (len (skills))
-#checar que tipo de datos son (lista)
+#checar que tipo de datos son (lista) pendiente
 
+
+#Modify the skills values by adding one or two skills
+estudiante = {
+    'first_name':'Jessica',
+    'last_name':'Mejia',
+    'age':18,
+    'country':'Mexico',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],   
+      'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+
+#Get the dictionary keys as a list
+estudiante = {
+    'first_name':'Jessica',
+    'last_name':'Mejia',
+    'age':18,
+    'country':'Mexico',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],   
+      'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+keys = estudiante.keys()
+print(keys)
+
+#Change the dictionary to a list of tuples using items() 
+estudiante = {
+    'first_name':'Jessica',
+    'last_name':'Mejia',
+    'age':18,
+    'country':'Mexico',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],   
+      'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+
+print(estudiante.items())
+
+#Delete one of the items in the dictionary
+estudiante = {
+    'first_name':'Jessica',
+    'last_name':'Mejia',
+    'age':18,
+    'country':'Mexico',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],   
+      'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+del estudiante['last_name']
+
+#Delete one of the dictionaries
+estudiante = {
+    'first_name':'Jessica',
+    'last_name':'Mejia',
+    'age':18,
+    'country':'Mexico',
+    'is_marred':False,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],   
+      'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+del estudiante
 
 
