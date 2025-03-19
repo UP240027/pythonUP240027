83301c77465acb9d0faf3d5c27fe36f90cb4f627
UP240027 <up240027@upa.alumnos.edu.mx>
2025-03-19 10:16:44 -0600

    .

--- 10_Day/03_Excersises.py
+++ 10_Day/03_Excersises.py
@@ -11,3 +11,13 @@ for fruta in reversed(lista):
     print("La lista de frutas al reves queda asi:", fruta)
 
 #Go to the data folder and use the countries_data.py file.Loop through the countries and extract all the countries containing the word land.
+import countries_data as cd
+paises = cd.countries
+for pais in paises:
+    if 'land' in pais:
+        print("Paises que llevan 'land' en su nombre :", pais) 
+
+#What are the total number of languages in the data
+import countries_data as cd
+paises = cd.countries
+total = 0
